f9cf887bbf0b3677ba8c74e665507d9d400793a5
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-10-27 10:53:01 +0600

    data successfully loaded

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const port = process.env.Port || 5000
 
 app.use(cors())
 
+const courses = require("./Data/course.json")
+
 app.get('/', (req, res) => {
     res.send('knowledge media server is running')
 })
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+
 
 app.listen(port, () => {
     console.log('knowledge media server running on port:', port)
